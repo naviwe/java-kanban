9baae2e805240489990142efdde600f6e0f26e50
Timur Novotorjin <timurnovotorzhin@yandex.kz>
2024-09-03 14:32:58 +0400

    fix: удалить не нужные импорты

--- src/test/InMemoryHistoryLinkedListTest.java
+++ src/test/InMemoryHistoryLinkedListTest.java
@@ -6,10 +6,9 @@ import task.Epic;
 import task.Status;
 import task.Subtask;
 import task.Task;
-import org.junit.jupiter.api.BeforeEach;
+
 import org.junit.jupiter.api.Test;
 
-import java.util.LinkedList;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
